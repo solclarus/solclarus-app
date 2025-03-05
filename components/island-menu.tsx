fd4f60e387f733da8ba784d5ae4fa22b9d544495
yoh <yokeus2000@gmail.com>
2025-03-05 22:02:54 +0900

    💄 Update style files

--- components/island-menu.tsx
+++ components/island-menu.tsx
@@ -1,3 +1,4 @@
+import { Separator } from "@/components/ui/separator";
 import { Book, Home, Rocket } from "lucide-react";
 
 import { IslandMenuButton } from "./island-menu-button";
@@ -7,10 +8,11 @@ import { ThemeSwitcher } from "./theme-switcher";
 
 export function IslandMenu() {
   return (
-    <nav className="z-100 fixed bottom-16 left-1/2 flex -translate-x-1/2 gap-2 rounded-full border bg-card p-2 shadow-lg md:bottom-20">
+    <nav className="fixed bottom-16 left-1/2 z-50 flex -translate-x-1/2 gap-1 rounded-full border bg-card p-2 shadow-lg md:bottom-20">
       <IslandMenuButton href={"/"} icon={<Home />} />
       <IslandMenuButton href={"/blog"} icon={<Book />} />
       <IslandMenuButton href={"/work"} icon={<Rocket />} />
+      <Separator orientation="vertical" className="mx-1 h-5 w-px self-center" />
       <ThemeSwitcher />
       <LangSwitcher />
       <ScrollToTop />
